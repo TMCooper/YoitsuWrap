4a312b2a4d0edb9497caa8f90bf6c5e3d49166ce
TMCooper <pokemon160377@gmail.com>
2026-08-18 00:00:01 +0200

    Corection de la mauvaise syntaxe et ajout de la variable API_LINK

--- src/YoitsuWrap/Config.py
+++ src/YoitsuWrap/Config.py
@@ -1,17 +1,18 @@
 class Config:
-    BASE_URL=str            # La base du lien de l'api a request (ex : http://127.0.0.1)
-    PORT=int                # Le port associer a l'api que l'on dois request (ex : 5000)
-    PATH=str                # Dossier racine ou ranger les données télécharger
-
+    BASE_URL: str            # La base du lien de l'api a request (ex : http://127.0.0.1)
+    PORT: int                # Le port associer a l'api que l'on dois request (ex : 5000)
+    PATH: str                # Dossier racine ou ranger les données télécharger
+    API_LINK: str            # Variable non demander a la construction de l'objet mais crée par déduction
+ 
     def __init__(self, BASE_URL: str, PORT: int, PATH: str): # Méthode de construction pour initialiser les variable de l'objet config
         """
         Constructeur de la class Config les arguments attendu sont : 
         - BASE_URL (str) : la base du lien api a request ex : http://127.0.0.1
         - PORT (int) : Le port associer a l'api ex : 5000
-        - PATH (str) : Dossier racine ou seront rangé les données télécharger"""
+        - PATH (str) : Dossier racine ou seront rangé les données télécharger
+        """
 
         self.BASE_URL = BASE_URL
         self.PORT = PORT
         self.PATH = PATH
-
-        return self
+        self.API_LINK = f"{BASE_URL}:{PORT}/api/"

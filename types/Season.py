2f8e2d075f8c78dd08e0cfe6fc837e593a7cab3d
TMCooper <pokemon160377@gmail.com>
2026-08-17 16:23:12 +0200

    Suppression de la methode construct

--- types/Season.py
+++ types/Season.py
@@ -1,18 +1,15 @@
 from Episode import Episode
 
 class Season:
-    number: str                 # Saison actuelle (ex : saison 1, saison 2, etc...)
+    saison: list[str]           # Saison actuelle (ex : saison 1, saison 2, etc...)
     title: str                  # Nom de la saison (ex : Frieren, Spice And Wolf, etc...)
     episode: list[Episode]      # Array des lien d'episode a télécharger
 
-    def __init__(self, number, title, episode):
-        self.number = number
+    def __init__(self, saison, title, episode): # Methode de creation de de l'objet complet season
+        self.saison = saison
         self.title = title
         self.episode = episode
 
-    def construct(self): # Methode de creation de de l'objet complet season
-        pass
-    
     def get_number(self): # Renvoie l'actuelle saison traité
         pass
 

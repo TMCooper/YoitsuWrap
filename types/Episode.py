ef63cf16933724c12e5475c0693ad723f2d03810
TMCooper <pokemon160377@gmail.com>
2026-08-17 16:19:59 +0200

    Revision des variable de base (passage en array)

--- types/Episode.py
+++ types/Episode.py
@@ -1,19 +1,17 @@
 from Config import Config
 
 class Episode:
-    title: str          # Titre de l'episode une fois télécharger (ex : ep 1, ep 2, etc...)
-    link: str           # Lien direct vers l'episode
-    path: str           # Path ou l'épisode sera ranger
-    number: int         # Number est le nombre de l'épisode souhaité (ex : 6 si on télécharge l'ep 5) 
+    title: str                      # Le nom de l'oeuvre que l'on souhaite traité (ex : Frieren)
+    title_file: list[str]           # Titre de l'episode une fois télécharger (ex : ep 1, ep 2, etc...)
+    link: list[str]                 # Lien direct vers l'episode
+    path: str                       # Path ou l'épisode sera ranger
+    number: list[int]                     # Number est le nombre de l'épisode souhaité (ex : 6 si on télécharge l'ep 5) 
 
-    def __init__(self, title, path, number):
+    def __init__(self, title, path, number): # Methode de contruction de l'objet Episode
         self.title = title
         self.link = "URL_FICTIF"        # get_link futur méthode de récuperation de lien
         self.path = path
         self.number = 3                 # Valeur fictive
-
-    def construct(self): # Methode de contruction de l'objet Episode
-        pass
 
     def get_title(self): # Renvoie le nom de l'épisode
         pass

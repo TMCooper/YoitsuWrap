1ddfedfa4a3c97ccc7b25c17e5acca75f1aaa169
TMCooper <pokemon160377@gmail.com>
2026-08-23 00:35:01 +0200

    Completion de la class Season & son Objet

--- src/YoitsuWrap/Season.py
+++ src/YoitsuWrap/Season.py
@@ -6,20 +6,22 @@ class Season:
     saison: str                         # Saison traité (ex : saison 1, saison 2, remake2024, etc...)
     title: str                          # Nom de la saison (ex : Frieren, Spice And Wolf, etc...)
     episodes: list[Episode]             # Array des lien d'episode a télécharger
-    episode_number: int                 # Nombre d'épisode stocker dans l'objet Season
+    episodes_numbers: int               # Nombre d'épisode stocker dans l'objet Season
     path: str                           # Path de rangement pour le chapitre (format : nom/chapter n/scan_x.jpg)
     api_link: str                       # Variable de stockage de l'api a requests
+    version: str                        # Version de l'animer traté par l'objet season (ex : vostfr, vf)
 
-    def __init__(self, saison, title, episodes, episode_number, path, api_link): # Methode de creation de de l'objet complet season
+    def __init__(self, saison, title, episodes, episodes_numbers, path, api_link, version): # Methode de creation de de l'objet complet season
         self.saison = saison
         self.title = title
         self.episodes = episodes
-        self.episode_number = episode_number
+        self.episodes_numbers = episodes_numbers
         self.path = path
         self.api_link = api_link
+        self.version = version
 
     @staticmethod
-    def get_season(title: str, saison: str, version: str, config: Config) -> list['Season']:
+    def search_by_name(title: str, saison: str, version: str, config: Config) -> 'Season':
         """
         Construction du dict d'objet Season arguement attendu :
         - title (str) : Titre de l'oeuvre (ex : Spice And Wolf)
@@ -28,13 +30,22 @@ class Season:
         - config (Config) : Objet config prealablement crée
         """
         api_link = config.API_LINK
-        pass
+        version = version.lower()
 
-    def get_episode_number(self):
+        episodes = []
+
+        base_data = requests.get(f"{api_link}/getSpecificAnime?q={title}&s={saison}&v={version}").json()
+        if base_data:
+            titre = base_data["title"]
+            episodes = Episode.search_by_name(title=titre, saison=saison, version=version, config=config)
+        objet_season = Season(saison=saison, title=titre, episodes=episodes, episodes_numbers=len(episodes), path=config.PATH, api_link=api_link, version=version)
+        return objet_season
+
+    def get_episodes_numbers(self):
         """
         Renvoie le nombre d'épisode stocker dans l'objet Season
         """
-        return len(self.episodes)
+        return self.episodes_numbers
 
     def get_title(self): # Renvoie le nom de la saison traité
         """
@@ -49,10 +60,28 @@ class Season:
         """
         Renvoie path configurer pour l'objet Season
         """
-        pass
+        return self.path
 
     def get_api_link(self):
         """
         Renvoie api_link configurer pour l'objet Season
         """
-        pass
+        return self.api_link
+
+    def get_episodes(self):
+        """
+        Renvoie l'arry d'objet episodes
+        """
+        return self.episodes
+
+    def get_version(self):
+        """
+        Renvoie la version de l'objet Season
+        """
+        return self.version
+
+    def get_saison(self):
+        """
+        Renvoie la saison de l'objet Season
+        """
+        return self.saison

79306bc0dca841a79b6892315ff659d20330aa56
TMCooper <pokemon160377@gmail.com>
2026-08-22 20:03:10 +0200

    Ajouts de episode_number changement de nom de methode & ecriture du system de téléchargement

--- src/YoitsuWrap/Episode.py
+++ src/YoitsuWrap/Episode.py
@@ -1,5 +1,5 @@
 from .Config import Config
-import requests, re
+import requests, os, yt_dlp
 
 class Episode:
     title: str                      # Le nom de l'oeuvre que l'on souhaite traité (ex : Frieren)
@@ -8,17 +8,19 @@ class Episode:
     api_link: str                   # Lien sur le quel l'objet est configurer
     season: str                     # Saison associer a l'épisode
     version: str                    # Version associer a la saison (ex : vostfr)
+    episode_number: int             # Numero de l'épisode contenue dans l'objet 
 
-    def __init__(self, title: str, link: str, path: str, api_link: str, season: str, version: str): # Methode de contruction des variable de base de l'objet Episode
+    def __init__(self, title: str, link: str, path: str, api_link: str, season: str, version: str, episode_number: int): # Methode de contruction des variable de base de l'objet Episode
         self.title = title
         self.link = link
         self.path = path
         self.api_link = api_link
         self.season = season
         self.version = version
+        self.episode_number = episode_number
 
     @staticmethod
-    def get_episode(title:str, saison: str, version: str, config: Config) -> list['Episode']: # Renvoie un dict des bjet episode pret a utilisation
+    def search_by_name(title:str, saison: str, version: str, config: Config) -> list['Episode']: # Renvoie un dict des bjet episode pret a utilisation
         """
         Construction du dict d'objet Episode arguement attendu :
         - title (str) : Titre de l'oeuvre (ex : Spice And Wolf)
@@ -34,11 +36,13 @@ class Episode:
         data = requests.get(f"{api_link}/getAnimeLink?n={title}&s={saison}&v={version}").json()
 
         objet_episode = []
+        i = 1
 
         if data:
             titre = base_data["title"]
             for donnee in data:
-                objet_episode.append(Episode(title=titre, link=donnee["url"], path=config.PATH, api_link=api_link,season=saison, version=version))
+                objet_episode.append(Episode(title=titre, link=donnee["url"], path=config.PATH, api_link=api_link,season=saison, version=version, episode_number=i))
+                i += 1
             return objet_episode
 
     def get_title(self) -> str:
@@ -59,9 +63,46 @@ class Episode:
         """
         return self.path
 
-    def download_episode(self, max_workers: int = 1) -> int:
+    def download_episode(self) -> int:
         """
-        Télécharge le(s) épisode(s) associer a l'objet épisode
-        - max_workers (int) : Nombre d'épisode a télécharger en même temps
+        Télécharge le épisodes associer a l'objet épisode
         """
-        pass
+        pre_path = os.path.join(self.path, self.title, self.season, self.version)
+
+        ydl_opts = {
+            "format": "best",                                                                           # Qualité vidéo maximale
+            "outtmpl": os.path.join(pre_path, f"{self.episode_number}.mp4"),                                        # Nom du fichier de sortie
+            "quiet": False,                                                                             # N'affiche pas les logs
+            "no_warning": True,                                                                         # Supprime les warnings
+            # "logger": cleanLogger,                                                                      # Logger personalisé
+            # "progress_hooks": [cleanLogger.hook],                                                       # Pour un affichage personnalisé de la progression
+            "http_headers": self.__get_headers(),                                                                # Header pour effectuer la requets
+        }
+        
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([self.link])
+
+    def __get_headers(self) -> dict:
+        headers = {
+                'authority': 'p16-ad-sg.tiktokcdn.com',
+                'method': 'GET',
+                'path': '/obj/ad-site-i18n-sg/202508125d0d6bceedbe1123419c9459',
+                'scheme': 'https',
+                'accept': '*/*',
+                'accept-encoding': 'gzip, deflate, br, zstd',
+                'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6,de;q=0.5,zh-CN;q=0.4,zh;q=0.3,ru;q=0.2,es;q=0.1,ko;q=0.1,vi;q=0.1,pl;q=0.1',
+                'cache-control': 'no-cache',
+                'origin': 'https://smoothpre.com',
+                'pragma': 'no-cache',
+                'priority': 'u=1, i',
+                'referer': 'https://smoothpre.com/',
+                'sec-ch-ua': '"Not)A;Brand";v="8", "Chromium";v="138", "Opera GX";v="122"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'cross-site',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36 OPR/122.0.0.0',
+                }
+
+        return headers

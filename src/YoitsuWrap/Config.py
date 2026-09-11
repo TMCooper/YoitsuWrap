473d9125aca1a0431fbab07135add2f5fda6cd29
TMCooper <pokemon160377@gmail.com>
2026-09-11 14:51:30 +0200

    Revision de la class config pour futur upgrade

--- src/YoitsuWrap/Config.py
+++ src/YoitsuWrap/Config.py
@@ -6,23 +6,32 @@ class Config:
     PATH: str                # Dossier racine ou ranger les données télécharger
     API_LINK: str            # Variable non demander a la construction de l'objet mais crée par déduction
  
-    def __init__(self, BASE_URL: str, PORT: int, PATH: str): # Méthode de construction pour initialiser les variable de l'objet config
-        """
-        Constructeur de la class Config les arguments attendu sont : 
-        - BASE_URL (str) : la base du lien api a request ex : http://127.0.0.1
-        - PORT (int) : Le port associer a l'api ex : 5000
-        - PATH (str) : Dossier racine ou seront rangé les données télécharger
-        """
-
+    def __init__(self, BASE_URL: str, PORT: int, PATH: str, API_LINK: str): # Méthode de construction pour initialiser les variable de l'objet config
         self.BASE_URL = BASE_URL
         self.PORT = PORT
         self.PATH = PATH
-        self.API_LINK = f"{BASE_URL}:{PORT}/api"
+        self.API_LINK = API_LINK
+
+
+    def create_config(url: str, port: int, path: str) -> 'Config':
+        """
+        Constructeur de la class Config 
+                
+        Args: 
+            BASE_URL (str) : la base du lien api a request ex : http://127.0.0.1
+            PORT (int) : Le port associer a l'api ex : 5000
+            PATH (str) : Dossier racine ou seront rangé les données télécharger
+                
+        Returns:
+            config (config)
+        """
+
+        API_LINK = f"{url}:{port}/api"
 
         try:
-            status = requests.get(f"{self.API_LINK}/status")
+            status = requests.get(f"{API_LINK}/status")
             if status:
-                pass
+                return Config(BASE_URL=url, PORT=port, PATH=path)
         except requests.exceptions.ConnectionError:
             print("Erreur reseau : Impossible de se connecter a l'api")
             exit()
